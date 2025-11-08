dc5470355859c159de1bb3df087f2ce6cc74aee1
hy-tran <tranbaohuy04@gmail.com>
2025-11-08 21:56:17 +0000

    User class

---
+++ backend/src/main/java/ie/tcd/scss/flight_scout/model/User.java
@@ -0,0 +1,28 @@
+package ie.tcd.scss.flight_scout.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data                     
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+}
+
