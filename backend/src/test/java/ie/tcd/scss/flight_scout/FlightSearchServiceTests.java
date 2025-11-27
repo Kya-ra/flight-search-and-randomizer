85dc3323f12f76429d2b3f4a672edeb70cc484e4
Theresa James <thjames@tcd.ie>
2025-11-27 22:08:28 +0000

    Added unit test for random flight suggester

--- backend/src/test/java/ie/tcd/scss/flight_scout/FlightSearchServiceTests.java
+++ backend/src/test/java/ie/tcd/scss/flight_scout/FlightSearchServiceTests.java
@@ -1,9 +1,14 @@
 package ie.tcd.scss.flight_scout;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import ie.tcd.scss.flight_scout.model.Flight;
 import ie.tcd.scss.flight_scout.model.FlightSearchResponse;
 import ie.tcd.scss.flight_scout.service.FlightSearchService;
 
@@ -33,5 +38,26 @@ class FlightSearchServiceTests {
         response.getFlights().forEach(f ->
             System.out.println(f.getDepartureTime() + " → " + f.getDestination() + " : " + f.getPrice())
         );
+    }
+
+    @Test
+    void testRandomFlight() {
+        Flight randomFlight = flightSearchService.getRandomFlight(
+        "DUB",         
+        "BCN",          
+        "2025-12-01",   
+        "2025-12-10",  
+        null,          
+        2,              
+        "EUR"       
+        );
+
+        assertNotNull(randomFlight, "Random flight should not be null");
+        assertEquals("DUB", randomFlight.getOrigin(), "Origin should be DUB");
+        assertEquals("BCN", randomFlight.getDestination(), "Destination should be BCN");
+        assertTrue(randomFlight.getPrice() > 0, "Price should be greater than 0");
+
+        System.out.println("Random flight selected:");
+        System.out.println(randomFlight.getDepartureTime() + " → " + randomFlight.getDestination() + " : " + randomFlight.getPrice());
     }
 }
